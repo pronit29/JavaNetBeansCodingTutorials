ed8524883ce22397d264cdf93ac6a7f0796b78a5
Pronit <pronit29@gmail.com>
2023-03-18 15:00:08 +0530

    My First Commit to this repository

---
+++ src/main/java/com/basic/codes/javatutorial/MyFirstJavaCode/MyFirstProgram.java
@@ -0,0 +1,17 @@
+/*
+ * My first java program to print Hello World in the console to verify if Java is setup and configured properly
+ */
+package com.basic.codes.javatutorial.MyFirstJavaCode;
+
+/**
+ *
+ * @author Pronit Kundu
+ */
+public class MyFirstProgram {
+    
+    public static void main(String args[]) {
+        
+        System.out.println("Hello Guys!!!!" + "\n" + "Welcome to the programming world of Java");
+    }
+    
+}
---
+++ src/main/java/com/mycompany/javatimbuchalkacodesround1/JavaTimBuchalkaCodesRound1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javatimbuchalkacodesround1;
+
+/**
+ *
+ * @author Pronit Kundu
+ */
+public class JavaTimBuchalkaCodesRound1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
