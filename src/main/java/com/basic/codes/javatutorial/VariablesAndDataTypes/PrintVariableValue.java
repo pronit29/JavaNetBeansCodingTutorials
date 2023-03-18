851f4a247a57d3b335f8cd9167d527e517b21966
Pronit <pronit29@gmail.com>
2023-03-18 15:00:08 +0530

    My First Commit to this repository

---
+++ src/main/java/com/basic/codes/javatutorial/VariablesAndDataTypes/PrintVariableValue.java
@@ -0,0 +1,18 @@
+/*
+ * Step 1 - Declare a variable of type int and assign a value to it
+ * Step 2 - Print the value of this variable in the console
+ */
+package com.basic.codes.javatutorial.VariablesAndDataTypes;
+
+/**
+ *
+ * @author Pronit Kundu
+ */
+public class PrintVariableValue {
+    
+    public static void main(String[] args) {
+        
+        int myFirstNumber = 5;
+        System.out.println("Value of the variable is: " + myFirstNumber);
+    }
+}
